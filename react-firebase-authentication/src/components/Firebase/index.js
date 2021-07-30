ff97860251494af884308c35a2474abfaee9deba
kikijinqili <jinqi.li.310@gmail.com>
2021-07-30 16:26:15 -0700

    intro route

---
+++ react-firebase-authentication/src/components/Firebase/index.js
@@ -0,0 +1,16 @@
+/*
+ * @Author: Jinqi Li
+ * @Date: 2021-07-30 13:30:22
+ * @LastEditors: Jinqi Li
+ * @LastEditTime: 2021-07-30 16:23:17
+ * @FilePath: \educativeProjects\react-firebase-authentication\src\components\Firebase\index.js
+ */
+import React from 'react';
+
+const Firebase = () => (
+  <div>
+    <h1>Firebase</h1>
+  </div>
+);
+
+export default Firebase;
---
+++ react-firebase-authentication/src/components/SignUp/index.js
@@ -0,0 +1,16 @@
+/*
+ * @Author: Jinqi Li
+ * @Date: 2021-07-30 13:32:46
+ * @LastEditors: Jinqi Li
+ * @LastEditTime: 2021-07-30 16:25:36
+ * @FilePath: \educativeProjects\react-firebase-authentication\src\components\SignUp\index.js
+ */
+import React from 'react';
+
+const SignUp = () => (
+  <div>
+    <h1>SignUp</h1>
+  </div>
+);
+
+export default SignUp;
